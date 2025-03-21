c685cd9902386c4d982497b7f4ab117f3d8606f5
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-22 00:29:55 +0530

    Variable In Go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var use create one or more variables
+
+	var a = "Nipun Anupama"
+	fmt.Println(a)
+
+	// You can declare multiple variables
+
+	var c, b int = 1, 2
+	fmt.Println(c, b)
+
+	//type of initialized variables
+
+	var x = true
+	fmt.Println(x)
+
+	// The `:=` syntax is shorthand for declaring and
+	// initializing a variable
+
+	n := "Apple "
+	fmt.Println(n)
+
+}
